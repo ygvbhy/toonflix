14fbcfd69089e79d82a23650bb00e461a1ca8169
bagsang-yun <ygvbhy@naver.com>
2023-06-19 17:33:48 +0900

    Pomodoros UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 import 'package:toonflix/widgets/button.dart';
 import 'package:toonflix/widgets/currency_card.dart';
 
@@ -14,6 +15,33 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class LargeTitle extends StatefulWidget {
+  const LargeTitle({super.key});
+
+  @override
+  State<LargeTitle> createState() => _LargeTitleState();
+}
+
+class _LargeTitleState extends State<LargeTitle> {
   bool showTitle = true;
 
   void toggleTitle() {
