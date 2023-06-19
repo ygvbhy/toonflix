c0640f0b3ad9d9b2f0ba7411b0a85d09de85e8b6
bagsang-yun <ygvbhy@naver.com>
2023-06-19 13:58:04 +0900

    state

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,61 @@ import 'package:toonflix/widgets/button.dart';
 import 'package:toonflix/widgets/currency_card.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
-class App extends StatelessWidget {
-  App({super.key});
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    counter = counter + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(
+                  Icons.add_box_rounded,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Wallet extends StatelessWidget {
+  Wallet({super.key});
 
   final List currency = ['Euro', 'Bitcoin', 'Dollar'];
   final List amount = ['6 428', '423', '55 627'];
